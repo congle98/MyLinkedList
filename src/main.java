b5c4dc1be9a515e7d56f722191e98164736b5a25
congle98 <lethanhcongcnhn@gmail.com>
2021-04-15 10:58:50 +0700

    add file

---
+++ src/main.java
@@ -0,0 +1,8 @@
+public class main {
+    public static void main(String[] args) {
+        MyLinkedList ll = new MyLinkedList();
+        ll.addFirst(1);
+        ll.addFirst(2);
+        ll.printList();
+    }
+}
